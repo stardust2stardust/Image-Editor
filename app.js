e428c5c34ab02aa335cb7d88a1102248f7e6eacd
Jeni Hoyt <ziahrahl@gmail.com>
2022-08-10 15:54:34 -0400

    get api w/ image display working

--- app.js
+++ app.js
@@ -2,22 +2,41 @@
 // Your API key: 563492ad6f917000010000013ffa5ca7846d48cfb03089f201d56f13
 
 
+
+const newImageBtn = document.getElementById('new-image-btn')
+newImageBtn.addEventListener('click', getImage)
+
 function imageInfo (data) {
-    console.log(data.photos);
-    return data.photos
+    console.log(data);
+    return data
 }
 
 function createImageObj (data) {
     console.log(data)
-    console.log(data[0].url)
+    // console.log(data.urls)
     imageObj = {}
-    imageObj.url = data[0].url
-    console.log(imageObj)
+    imageObj.url = data.urls.small
+    // imageObj.title = data[0].alt
+    // imageObj.photographer = data[0].photographer
+    // imageObj.src = data[0].src
+    // console.log(imageObj)
+    return imageObj
 }
 
-const newImage = fetch('https://api.pexels.com/v1/search?query=nature&per_page=1');
-newImage
-.then(response => response.json())
-.then(imageInfo)
-.then(createImageObj)
+function displayImage(imageObj) {
+    console.log(imageObj)
+    const imgContainer = document.querySelector('.img-container')
+    const image = document.createElement('img');
+    image.src = imageObj.url
+    imgContainer.appendChild(image)
+}
+
+function getImage () {
+    fetch('https://api.unsplash.com/photos/random/?client_id=8_R28_vR62gV7KKgNX4LzUQ3myYfMb6GkH0N2yDCpXo', {mode: 'cors'})
+    .then(response => response.json())
+    .then(imageInfo)
+    .then(createImageObj)
+    .then(displayImage)
+}
+
 
